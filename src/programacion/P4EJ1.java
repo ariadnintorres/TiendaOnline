e6e358c7f0cca8bad61beb9a6d9d06ebde43a788
ariadnintorres <ariadnintorres@gmail.com>
2025-12-04 17:27:21 +0100

    Primer commit - Subiendo la base del proyecto Tienda Online

---
+++ src/programacion/P4EJ1.java
@@ -0,0 +1,16 @@
+package programacion;
+import java.util.Scanner;
+public class P4EJ1 {
+
+	public static void main(String[] args) {
+		System.out.println("Bienvenido a la Tienda Online ");
+	System.out.println("");
+		String productos[];
+		
+		
+		
+		
+
+	}
+
+}
